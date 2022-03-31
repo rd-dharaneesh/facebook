b68977ac98cd58150569c0676e8fcf2635c0c9ed
dharaneesh <ratakondadharaneesh@gmail.com>
2022-03-31 12:37:34 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-this line i have to keep
-
-=======
-testing stash commmand
-this is the first line
->>>>>>> fba3e53 (updated)
->>>>>>> developers
 again updated this file
 this os updated
 one more time it is updated
